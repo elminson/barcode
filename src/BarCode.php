ce2348bf851fa207d8c08031d52d40d74ccd7809
Elminson De Oleo Baez <edeoleo@gmail.com>
2018-09-15 03:04:33 -0400

    Adding

--- src/BarCode.php
+++ src/BarCode.php
@@ -19,11 +19,11 @@ class BarCode
     protected $SizeFactor = 1;
     protected $code_array = 'code128';
 
-    public function __construct()
+    public function __construct($code = 'code128')
     {
-        $this->code_type = "code128";
-        $this->code_array = "code128";
-        $this->setCode_type("code128");
+        $this->code_type = $code;
+        $this->code_array = $code;
+        $this->setCode_type($code);
     }
 
     public function getFilepath()
@@ -102,23 +102,27 @@ class BarCode
      */
     public function generate($text)
     {
-        $code_array='code128';
-        $code_type='code128';
-        if (in_array(strtolower($code_type), array("code128", "code128b"))) {
-            $chksum = 104;
-            // Must not change order of array elements as the checksum depends on the array's key to validate final code
-            CodeType::code_data();
-            $code_keys = array_keys($code_array);
-            $code_values = array_flip($code_keys);
-            for ($X = 1; $X <= strlen($text); $X++) {
-                $activeKey = substr($text, ($X - 1), 1);
-                $code_string .= $code_array[$activeKey];
-                $chksum = ($chksum + ($code_values[$activeKey] * $X));
-            }
-            $code_string .= $code_array[$code_keys[($chksum - (intval($chksum / 103) * 103))]];
-
-            $code_string = "211214" . $code_string . "2331112";
+        $this->setText($text);
+        if ($this->code_type == 'code128a') {
+            $this->text = strtoupper($this->text);
         }
+
+        //if (in_array(strtolower($code_type), array("code128", "code128b"))) {
+        $chksum = (new CodeType)->getChecksum($this->getCode_type());
+        // Must not change order of array elements as the checksum depends on the array's key to validate final code
+        $code_array = (new CodeType)->getCode($this->getCode_type());
+        $code_keys = array_keys($code_array);
+        $code_values = array_flip($code_keys);
+        $code_string = "";
+        for ($X = 1; $X <= strlen($text); $X++) {
+            $activeKey = substr($text, ($X - 1), 1);
+            $code_string .= $code_array[$activeKey];
+            $chksum = ($chksum + ($code_values[$activeKey] * $X));
+        }
+        $code_string .= $code_array[$code_keys[($chksum - (intval($chksum / 103) * 103))]];
+
+        $code_string = "211214" . $code_string . "2331112";
+
     }
 
     function barcode(
